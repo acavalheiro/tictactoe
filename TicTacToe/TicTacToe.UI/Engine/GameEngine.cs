96b47ad27e89ea17640da6fe797dca49668b0109
André Filipe Cavalheiro <andre.cavalheiro@thevirtualforge.com>
2023-04-10 16:14:57 +0100

    last changes

--- TicTacToe/TicTacToe.UI/Engine/GameEngine.cs
+++ TicTacToe/TicTacToe.UI/Engine/GameEngine.cs
@@ -1,47 +1,112 @@
-﻿using System.Numerics;
+﻿using System.Runtime.CompilerServices;
 
 namespace TicTacToe.UI.Engine
 {
     public class Player
     {
-        public Player(string name, char tag)
+        public Player(string name, string tag)
         {
             this.Name = name;
             this.Tag = tag;
         }
         public string Name { get; set; }
-        public char Tag { get; set; }
+        public string Tag { get; set; }
+
+        public int Wins { get; set; }
 
     }
     public class GameEngine
     {
-        private Player playerOne;
-        private Player playerTwo;
+        public Player? playerOne;
+        public Player? playerTwo;
 
-        public string[,] values = new string[3, 3];
+        private Player? currentPlayer;
 
-        bool player;
+        public string[,] Board => this._board;
+
+        private string[,] _board = new string[3, 3];
+
+        private bool _player;
+
+        private int _currentMoves = 0;
+
+        public string? GameStatus { get; set; }
 
         public void PlayerCall(int x, int y)
         {
-            if (!string.IsNullOrEmpty(values[x, y]))
+            if (!string.IsNullOrEmpty(_board[x, y]))
                 return;
 
-            values[x, y] = player ? "X" : "0";
-            player = !player;
+            _board[x, y] = this.currentPlayer.Tag;
+            _currentMoves++;
+
+            if (this.CheckForWin())
+            {
+                if (this.currentPlayer.Tag == this.playerOne.Tag)
+                    this.playerOne.Wins++;
+                else
+                    this.playerTwo.Wins++;
+
+                this.SetGameStatus(true);
+                return;
+            }
+
+            this.SetCurrentPlayer();
+
+            this.SetGameStatus();
         }
 
         public bool IsLocked(int x, int y)
         {
-            return !string.IsNullOrEmpty(values[x, y]);// ? string.Empty : "cannotuse";
+            return !string.IsNullOrEmpty(_board[x, y]);
         }
 
-        public void Restart() => values = new string[3, 3];
 
         public void SetupPlayers(string playerOne, string playerTwo)
         {
-            this.playerOne = new Player(playerOne, 'X');
-            this.playerTwo = new Player(playerTwo, '0');
+            this.playerOne = new Player(playerOne, "X");
+            this.playerTwo = new Player(playerTwo, "0");
+            this.Restart();
+        }
+
+        public void Restart()
+        {
+            _board = new string[3, 3];
+            this._currentMoves = 0;
+            this.SetCurrentPlayer();
+            this.SetGameStatus();
+        }
+
+        private void SetCurrentPlayer()
+        {
+            this.currentPlayer = _player ? playerOne : playerTwo;
+            _player = !_player;
+        }
+
+        private void SetGameStatus(bool isWinner = false)
+        {
+            this.GameStatus = isWinner ? $"Player {this.currentPlayer.Name} ({this.currentPlayer.Tag}) Wins!" : $"Player {this.currentPlayer.Name} ({this.currentPlayer.Tag}) to play!";
+        }
+
+        private bool CheckForWin()
+        {
+            if (this._currentMoves < 5) return false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (_board[i, 0] == this.currentPlayer.Tag && _board[i, 1] == this.currentPlayer.Tag && _board[i, 2] == this.currentPlayer.Tag)
+                {
+                    return true;
+                }
+
+                if (_board[0, i] == this.currentPlayer.Tag && _board[1, i] == this.currentPlayer.Tag && _board[2, i] == this.currentPlayer.Tag)
+                {
+                    return true;
+                }
+            }
+
+            return (_board[0, 0] == this.currentPlayer.Tag && _board[1, 1] == this.currentPlayer.Tag && _board[2, 2] == this.currentPlayer.Tag) ||
+                (_board[0, 2] == this.currentPlayer.Tag && _board[1, 1] == this.currentPlayer.Tag && _board[2, 0] == this.currentPlayer.Tag);
         }
     }
 }
