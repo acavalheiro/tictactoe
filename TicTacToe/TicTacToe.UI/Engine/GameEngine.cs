d976172b3d093c148f5ace14d019c0ed1d3b773e
André Filipe Cavalheiro <andre.cavalheiro@thevirtualforge.com>
2023-04-10 13:07:32 +0100

    Changes

---
+++ TicTacToe/TicTacToe.UI/Engine/GameEngine.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+namespace TicTacToe.UI.Engine
+{
+    public class Player
+    {
+        public Player(string name, char tag)
+        {
+            this.Name = name;
+            this.Tag = tag;
+        }
+        public string Name { get; set; }
+        public char Tag { get; set; }
+
+    }
+    public class GameEngine
+    {
+        private Player playerOne;
+        private Player playerTwo;
+
+        public string[,] values = new string[3, 3];
+
+        bool player;
+
+        public void PlayerCall(int x, int y)
+        {
+            if (!string.IsNullOrEmpty(values[x, y]))
+                return;
+
+            values[x, y] = player ? "X" : "0";
+            player = !player;
+        }
+
+        public bool IsLocked(int x, int y)
+        {
+            return !string.IsNullOrEmpty(values[x, y]);// ? string.Empty : "cannotuse";
+        }
+
+        public void Restart() => values = new string[3, 3];
+
+        public void SetupPlayers(string playerOne, string playerTwo)
+        {
+            this.playerOne = new Player(playerOne, 'X');
+            this.playerTwo = new Player(playerTwo, '0');
+        }
+    }
+}
--- TicTacToe/TicTacToe.UI/Program.cs
+++ TicTacToe/TicTacToe.UI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TicTacToe.UI;
+using TicTacToe.UI.Engine;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new GameEngine());
 
 await builder.Build().RunAsync();
 
